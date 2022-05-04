e0c95f62f776222f78d2d63c9e6d71b4dcc76495
neilson <joseneilson25@gmail.com>
2022-05-03 21:52:48 -0300

    Contexto de autenticação

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,29 @@
-import{ FormEvent, useState } from 'react'
+import{ FormEvent, useContext, useState } from 'react'
+import { AuthContext } from '../contexts/AuthContext';
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
 const [email, setEmail] = useState('');
 const [password, setPassword] = useState('');
 
-function handleSubmit(event: FormEvent) {
+const { signIn } = useContext(AuthContext)
+
+async function handleSubmit(event: FormEvent) {
+
+  event.preventDefault();
+
 const data ={
     email,
     password,
   }
 
-  console.log(data);
+  await signIn(data);
 }
 return (
     <form onSubmit={handleSubmit} className={styles.container}>
-    <input type="email" value={email} onChange={e=> setEmail(e.target.value)} />
-    <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
-    <button type="submit">Entrar</button>
+        <input type="email" value={email} onChange={e=> setEmail(e.target.value)} />
+        <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
+        <button type="submit">Entrar</button>
     </form>
 )
 }
